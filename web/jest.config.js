b861035e24d67fdab515cfba9bdbf8d8788ac388
An Cao <kcj.pop.com@gmail.com>
2022-03-22 22:13:04 +0200

    Re-organize tests
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/jest.config.js
+++ web/jest.config.js
@@ -13,6 +13,7 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/__data__/'],
   moduleNameMapper: {
     '^@/lib/(.*)$': '<rootDir>/lib/$1',
   },
---
+++ web/lib/__tests__/__data__/affirm.js
@@ -0,0 +1,5 @@
+export const input = `@affirm<br>- khẳng định`
+export const output = {
+  word: 'affirm',
+  definitions: [{ definitions: [{ meaning: 'khẳng định' }] }],
+}
