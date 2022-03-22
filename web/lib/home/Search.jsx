3b315e4c1ba498e88098e941dda1097d6f6e7975
An Cao <kcj.pop.com@gmail.com>
2022-03-22 22:04:02 +0200

    Search from user input
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/lib/home/Search.jsx
+++ web/lib/home/Search.jsx
@@ -1,11 +1,13 @@
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 
 export function Search() {
+  const [keyword, setKeyword] = useState('')
   const router = useRouter()
 
   const doSubmit = (e) => {
     e.preventDefault()
-    router.push('/w/actuate')
+    router.push(`/w/${keyword.trim()}`)
   }
 
   return (
@@ -14,6 +16,7 @@ export function Search() {
         type="search"
         placeholder="Nhập từ cần tra"
         className="block w-full rounded-md border border-gray-300 p-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        onChange={(e) => setKeyword(e.target.value)}
       />
     </form>
   )
