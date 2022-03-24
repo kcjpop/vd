ab3ece865807981cd36aa5f9c843160c672f30a6
An Cao <kcj.pop.com@gmail.com>
2022-03-24 16:09:35 +0200

    Debug errors
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/pages/api/words/[word].js
+++ web/pages/api/words/[word].js
@@ -9,6 +9,7 @@ export default async function handler(req, res) {
 
     res.status(200).json(result)
   } catch (err) {
+    console.error(err)
     res.status(500).json({ error: 'failed to load data' })
   }
 }
