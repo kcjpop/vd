01b5c71b7d93ff5273a5cd532c6138c67f65d7c3
Dang Min <stea.steven@gmail.com>
2022-03-24 23:49:26 +0200

    Incorporate en-vi.db
    - Copy en-vi.db into web/db
    - Add `getDefinitions` function: find word from db and parse its glossary into defintions field

---
+++ web/lib/domain-logic/definitions.js
@@ -0,0 +1,20 @@
+import { getDb } from './db'
+import { parse } from '@/lib/parser'
+
+const EN_VI_DB = 'en-vi.db'
+
+/**
+ * Get word from db
+ * @param {string} word
+ */
+export async function getDefinitions(word) {
+  const db = await getDb(EN_VI_DB)
+
+  const entry = await db.get('SELECT glossary FROM words WHERE word = :word', {
+    ':word': word,
+  })
+
+  if (entry.glossary && entry.glossary !== '') {
+    return parse(entry.glossary)
+  }
+}
--- web/pages/api/words/[word].js
+++ web/pages/api/words/[word].js
@@ -1,11 +1,14 @@
 import { getAllTenses } from '@/lib/domain-logic/verbTenses'
+import { getDefinitions } from '@/lib/domain-logic/definitions'
 
 export default async function handler(req, res) {
   try {
     const tenses = await getAllTenses(req.query.word)
+    const definitions = await getDefinitions(req.query.word)
 
     const result = {}
     result.tenses = tenses ?? null
+    result.defintions = definitions ?? null
 
     res.status(200).json(result)
   } catch (err) {
