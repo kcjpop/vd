b39f9de7bace08e041444849896c369c1f591eb1
An Cao <kcj.pop.com@gmail.com>
2022-03-25 22:50:46 +0200

    Change style of search input in frontpage
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/lib/components/common/Icons.jsx
+++ web/lib/components/common/Icons.jsx
@@ -1,4 +1,5 @@
 export {
   FiVolume2 as VolumnIcon,
   FiSettings as SettingsIcon,
+  FiSearch as SearchIcon,
 } from 'react-icons/fi'
--- web/lib/components/home/Search.jsx
+++ web/lib/components/home/Search.jsx
@@ -1,26 +1,33 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 
+import { SearchIcon } from '../common/Icons'
+
 export function Search() {
   const [keyword, setKeyword] = useState('')
   const router = useRouter()
 
   const doSubmit = (e) => {
     e.preventDefault()
-    router.push(`/w/${keyword.trim().toLocaleLowerCase()}`)
+    const k = keyword.trim().toLocaleLowerCase()
+    if (k !== '') router.push(`/w/${k}`)
   }
 
   return (
-    <form onSubmit={doSubmit}>
+    <form onSubmit={doSubmit} className="relative">
       <input
         name="keyword"
         id="keyword"
         autoComplete="en"
         type="search"
+        required
         placeholder="Nhập từ cần tra"
-        className="block w-full rounded-md border border-gray-300 p-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+        className="w-full rounded-md border border-slate-300 bg-white py-4 pl-4 pr-10 placeholder-slate-400 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-1 focus:ring-pink-500"
         onChange={(e) => setKeyword(e.target.value)}
       />
+      <button type="submit" className="absolute inset-y-0 right-0 p-4">
+        <SearchIcon />
+      </button>
     </form>
   )
 }
