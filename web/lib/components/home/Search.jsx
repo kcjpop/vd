150bd25a9c1f39e9c0b2940ccd612ba06a3353aa
An Cao <kcj.pop.com@gmail.com>
2022-03-25 15:14:41 +0200

    Attempt to show keyword suggestions on mobile
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/lib/components/home/Search.jsx
+++ web/lib/components/home/Search.jsx
@@ -13,6 +13,9 @@ export function Search() {
   return (
     <form onSubmit={doSubmit}>
       <input
+        name="keyword"
+        id="keyword"
+        autoComplete="en"
         type="search"
         placeholder="Nhập từ cần tra"
         className="block w-full rounded-md border border-gray-300 p-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
