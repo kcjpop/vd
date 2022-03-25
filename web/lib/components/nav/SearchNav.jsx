68ce7b5462adce1c542f3e16f095eb5aa94dd943
An Cao <kcj.pop.com@gmail.com>
2022-03-26 00:32:17 +0200

    Attempt to avoid browser zooming on mobile
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/lib/components/nav/SearchNav.jsx
+++ web/lib/components/nav/SearchNav.jsx
@@ -27,7 +27,7 @@ function SearchForm() {
         type="search"
         required
         placeholder="Nhập từ cần tra"
-        className="h-8 w-full rounded-full bg-slate-200 pl-4 text-sm placeholder:text-slate-500 focus:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-sky-500"
+        className="h-8 w-full rounded-full bg-slate-200 pl-4 placeholder:text-slate-500 focus:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-sky-500"
       />
 
       <button type="submit" className="absolute inset-y-0 right-0 px-2">
