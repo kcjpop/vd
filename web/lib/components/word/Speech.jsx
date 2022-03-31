b1782c955241e9524f45fbf5e660a811a07c4684
Dang Min <stea.steven@gmail.com>
2022-03-31 11:56:23 +0300

    Hide speaker icon until voices are loaded. (#25)
    * Hide speaker icon until voices are loaded.
    
    Use `addEventListener` in `useEffect` to update state of voices, `removeEventListener` to cleanup the `useEffect` afterward.

--- web/lib/components/word/Speech.jsx
+++ web/lib/components/word/Speech.jsx
@@ -1,26 +1,47 @@
+import React from 'react'
 import { getVoiceSettings } from '../../storage'
 import { VolumnIcon } from '../common/Icons'
 
-const LANG = 'en-GB'
+const LANG = 'en'
 
 export function Speech({ word }) {
-  const doSpeak = () => {
-    const selectedVoice = getVoiceSettings().get()
+  const [voices, setVoices] = React.useState(null)
 
+  React.useEffect(() => {
+    const doGetVoices = () => {
+      const synth = window.speechSynthesis
+      const voices = synth.getVoices()
+
+      if (voices.length > 0) {
+        setVoices(voices.filter(({ lang }) => lang.startsWith(LANG)))
+      }
+    }
+
+    doGetVoices()
+
+    window.speechSynthesis.addEventListener('voiceschanged', doGetVoices)
+
+    return () => {
+      window.speechSynthesis.removeEventListener('voiceschanged', doGetVoices)
+    }
+  }, [])
+
+  const doSpeak = () => {
     const synth = window.speechSynthesis
-    const voice = synth
-      .getVoices()
-      .find((voice) =>
-        selectedVoice ? voice.voiceURI === selectedVoice : voice.lang === LANG,
-      )
+    const utterance = new SpeechSynthesisUtterance(word)
+    const selected = getVoiceSettings().get()
+    const voice = voices.find(({ lang, voiceURI }) =>
+      selected ? selected === voiceURI : lang.startsWith(LANG),
+    )
 
     if (voice) {
-      const utterance = new SpeechSynthesisUtterance(word)
       utterance.lang = voice.lang
       utterance.voice = voice
       synth.speak(utterance)
     }
   }
 
-  return <VolumnIcon size="24px" className="cursor-pointer" onClick={doSpeak} />
+  return !voices ? null : (
+    <VolumnIcon size="24px" className="cursor-pointer" onClick={doSpeak} />
+  )
 }
