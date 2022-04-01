9d5b1a672259b48e9284859632e6289ae27c2ec2
Dang Min <dang.min@tecnotree.com>
2022-04-01 10:40:36 +0300

    Try using getServerSideProps to create opengraph object.

--- web/pages/w/[word].jsx
+++ web/pages/w/[word].jsx
@@ -1,3 +1,13 @@
 import { Page } from '@/lib/components/word'
 
 export default Page
+
+export async function getServerSideProps(ctx) {
+  const { word } = ctx.query
+
+  const opengraph = {
+    title: `“${word}” tiếng Việt là gì?`,
+  }
+
+  return { props: { opengraph } }
+}
