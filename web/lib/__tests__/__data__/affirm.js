d537378516c87708d51b03655f8d5f5b9eed5e7c
An Cao <kcj.pop.com@gmail.com>
2022-04-02 16:28:52 +0300

    Remove tests
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/lib/__tests__/__data__/affirm.js
+++
@@ -1,5 +0,0 @@
-export const input = `@affirm<br>- khẳng định`
-export const output = {
-  word: 'affirm',
-  definitions: [{ definitions: [{ meaning: 'khẳng định' }] }],
-}
--- web/lib/__tests__/parser.test.js
+++
@@ -1,9 +0,0 @@
-import { parse } from '../parser'
-
-import * as all from './__data__/all'
-import * as affirm from './__data__/affirm'
-
-xtest('parse', () => {
-  expect(parse(all.input)).toEqual(all.output)
-  expect(parse(affirm.input)).toEqual(affirm.output)
-})
