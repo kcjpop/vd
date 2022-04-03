5a2a8759c8c2010c86be551ba54c0d0da328c5a0
An Cao <kcj.pop.com@gmail.com>
2022-04-03 13:57:53 +0300

    Fix style if a word has many pronunciations
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/lib/components/word/Pronunciations.jsx
+++ web/lib/components/word/Pronunciations.jsx
@@ -2,7 +2,7 @@ export function Pronunciations({ entries }) {
   if (!entries) return null
 
   return (
-    <div className="text-slate-700">
+    <div className="flex flex-wrap gap-2 text-slate-700">
       {entries.map((p) => (
         <p key={p} className="tracking-wider">
           /{p}/
