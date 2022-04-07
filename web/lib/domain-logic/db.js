75cc87baf6f887d68ffe0669d3f0e06c0fe357b9
An Cao <kcj.pop.com@gmail.com>
2022-04-07 10:39:38 +0300

    Remove more stuff
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/lib/domain-logic/db.js
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-import Database from 'better-sqlite3'
-
-export function getDb(dbName) {
-  if (!dbName) throw new Error('Database name is required')
-
-  const filename = path.join(process.cwd(), 'db/' + dbName)
-  return new Database(filename, { readonly: true })
-}
