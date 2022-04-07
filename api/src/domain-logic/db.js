b8c56a8b0545c40c352248ae59f137329eeb00dd
An Cao <kcj.pop.com@gmail.com>
2022-04-07 21:40:24 +0300

    Re-arrange backend
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- api/src/domain-logic/db.js
+++ api/src/domain-logic/db.js
@@ -1,9 +1,9 @@
 const path = require('path')
 const Database = require('better-sqlite3')
 
-exports.getDb = function getDb(dbName) {
+exports.getDb = function getDb(dbName, options) {
   if (!dbName) throw new Error('Database name is required')
 
-  const filename = path.join(process.cwd(), 'src/db/' + dbName)
-  return new Database(filename, { readonly: true })
+  const filename = path.join(process.cwd(), 'db/' + dbName)
+  return new Database(filename, { readonly: true, ...options })
 }
