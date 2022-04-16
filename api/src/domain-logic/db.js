4588a7425d440b54c75f521acca9dd121b047ea3
An Cao <kcj.pop.com@gmail.com>
2022-04-16 09:50:36 +0300

    Define db-test
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- api/src/domain-logic/db.js
+++ api/src/domain-logic/db.js
@@ -4,6 +4,8 @@ const Database = require('better-sqlite3')
 exports.getDb = function getDb(dbName, options) {
   if (!dbName) throw new Error('Database name is required')
 
-  const filename = path.join(process.cwd(), 'db/' + dbName)
+  const dbFolder = process.env.NODE_ENV !== 'production' ? 'db-test' : 'db'
+
+  const filename = path.join(process.cwd(), dbFolder, dbName)
   return new Database(filename, { readonly: true, ...options })
 }
