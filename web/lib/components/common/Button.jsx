3b6b14b4541a096e44d1d862626a69dc12befa56
Dang Min <dang.min@tecnotree.com>
2022-04-19 08:21:40 +0300

    Modify Button component.

--- web/lib/components/common/Button.jsx
+++ web/lib/components/common/Button.jsx
@@ -1,10 +1,40 @@
-export function Button({ children, className, ...props }) {
+import { useFloating, shift } from '@floating-ui/react-dom-interactions'
+
+export function Button({
+  children,
+  className,
+  tooltip,
+  variant = 'text',
+  type = 'primary',
+  ...props
+}) {
+  const {
+    x,
+    y,
+    reference: ref,
+    floating,
+    strategy,
+  } = useFloating({
+    placement: 'top',
+    middleware: [shift()],
+  })
+
   return (
-    <button
-      type="button"
-      className={`rounded-lg px-6 py-2 ${className}`}
-      {...props}>
-      {children}
-    </button>
+    <>
+      <button
+        type="button"
+        ref={ref}
+        className={`rounded-lg px-6 py-2 btn--${type} btn--${variant} ${className}`}
+        {...props}>
+        {children}
+      </button>
+      {tooltip && (
+        <div
+          ref={floating}
+          style={{ position: strategy, top: y ?? '', left: x ?? '' }}>
+          {tooltip}
+        </div>
+      )}
+    </>
   )
 }
