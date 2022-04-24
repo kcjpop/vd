7398b04b4a78640447d5b29999aba13d0bbce463
An Cao <kcj.pop.com@gmail.com>
2022-04-24 10:55:24 +0300

    Add loading state for Button
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/lib/components/common/Button.jsx
+++ web/lib/components/common/Button.jsx
@@ -1,10 +1,24 @@
-export function Button({ children, className, type = 'button', ...props }) {
+import { Spinner } from './Spinner'
+
+export function Button({
+  children,
+  className,
+  type = 'button',
+  loading = false,
+  ...props
+}) {
   return (
     <button
       type={type}
       className={`rounded px-4 py-2 disabled:!bg-slate-200 disabled:!opacity-60 ${className}`}
       {...props}>
-      {children}
+      {loading ? (
+        <div className="flex items-center justify-center">
+          <Spinner />
+        </div>
+      ) : (
+        children
+      )}
     </button>
   )
 }
