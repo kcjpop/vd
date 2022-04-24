31822198d54dfde49be78c244c0de4093c1f90e9
An Cao <kcj.pop.com@gmail.com>
2022-04-24 09:30:58 +0300

    Supabase should throw errors
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- web/lib/supabaseClient.js
+++ web/lib/supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL
 const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(url, anonKey)
+export const supabase = createClient(url, anonKey, { shouldThrowOnError: true })
