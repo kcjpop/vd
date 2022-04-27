2715fbdc0d15b282715c4af093a58ea55a26c612
Dang Min <dang.min@tecnotree.com>
2022-04-27 11:35:03 +0300

    Adjust notify function.

--- web/lib/context/Toast.jsx
+++ web/lib/context/Toast.jsx
@@ -8,23 +8,29 @@ export const ToastContextProvider = ({ children }) => {
     title: null,
     description: null,
     action: null,
+    actionProps: {},
     variant: 'success',
   })
 
   const openChange = (state) => setProps({ ...props, open: state })
 
+  /* If you would like to use action, remember to add actionProps
+   * ActionProps: { asChild?: boolean; altText: string }
+   */
   const notify = ({
     title,
     description,
+    duration,
     action = null,
+    actionProps = {},
     type = 'background',
-    duration = 3000,
     variant = 'success',
   }) => {
     setProps({
       title,
       description,
       action,
+      actionProps,
       type,
       duration,
       open: true,
