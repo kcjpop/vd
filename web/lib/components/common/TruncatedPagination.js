aec88f58a15a9921c326da15ac86697f574ebdc7
An Cao <kcj.pop.com@gmail.com>
2022-04-27 09:10:36 +0300

    Show truncated pagination
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

---
+++ web/lib/components/common/TruncatedPagination.js
@@ -0,0 +1,76 @@
+import clsx from 'clsx'
+
+/**
+ * Based on this gist
+ * https://gist.github.com/kottenator/9d936eb3e4e3c3e02598
+ */
+
+const DOTS = '…'
+
+function getRange(start, end) {
+  return Array.from({ length: end - start + 1 }, (_, i) => i + start)
+}
+
+function pagination(current, length, delta = 4) {
+  const range = {
+    start: Math.round(current - delta / 2),
+    end: Math.round(current + delta / 2),
+  }
+
+  if (range.start - 1 === 1 || range.end + 1 === length) {
+    range.start += 1
+    range.end += 1
+  }
+
+  let pages =
+    current > delta
+      ? getRange(
+          Math.min(range.start, length - delta),
+          Math.min(range.end, length),
+        )
+      : getRange(1, Math.min(length, delta + 1))
+
+  const withDots = (value, pair) =>
+    pages.length + 1 !== length ? pair : [value]
+
+  if (pages[0] !== 1) {
+    pages = withDots(1, [1, DOTS]).concat(pages)
+  }
+
+  if (pages[pages.length - 1] < length) {
+    pages = pages.concat(withDots(length, [DOTS, length]))
+  }
+
+  return pages
+}
+
+function DefaultLinkComponent({ page, currentPage }) {
+  return (
+    <span
+      className={clsx({
+        'font-bold': currentPage === page,
+      })}>
+      {page}
+    </span>
+  )
+}
+
+export function TruncatedPagination({
+  currentPage,
+  totalPage,
+  LinkComponent = DefaultLinkComponent,
+}) {
+  const pages = pagination(currentPage, totalPage)
+
+  return (
+    <div className="flex items-center justify-between gap-2">
+      {pages.map((page, i) => (
+        <LinkComponent
+          key={page + i}
+          page={page !== DOTS ? page : null}
+          currentPage={currentPage}
+        />
+      ))}
+    </div>
+  )
+}
--- web/lib/components/dict/Page.js
+++ web/lib/components/dict/Page.js
@@ -1,10 +1,42 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { TruncatedPagination } from '../common/TruncatedPagination'
 import { Layout } from '../common/Layout'
 import { Select } from '../common/Select'
 import { LinkToWord } from '../common/LinkToWord'
-import { useRouter } from 'next/router'
+
+function PageLink({ page, currentPage }) {
+  const router = useRouter()
+  const newOffset = (page - 1) * Number(router.query.limit)
+
+  if (page === null) return <span className="text-slate-400">…</span>
+
+  return (
+    <Link
+      href={{
+        pathname: router.pathname,
+        query: { ...router.query, offset: newOffset },
+      }}>
+      <a
+        className={clsx(
+          'rounded bg-orange-100 px-4 py-2 text-sm tracking-wide text-orange-700',
+          {
+            'font-bold': currentPage === page,
+          },
+        )}>
+        {page}
+      </a>
+    </Link>
+  )
+}
 
 export function PageDict({ words, total, offset, limit }) {
   const router = useRouter()
+
+  const totalPage = Math.ceil(total / limit)
+  const currentPage = Math.floor(offset / limit) + 1
 
   const doChangeLimit = (e) => {
     router.push({
@@ -15,35 +47,51 @@ export function PageDict({ words, total, offset, limit }) {
 
   return (
     <Layout>
-      <h1 className="mb-4 text-4xl font-bold">Từ điển Anh Việt</h1>
-      <div className="mb-4 flex items-center justify-between">
-        <span>
-          Showing{' '}
-          <strong className="font-bold">
-            {offset} - {offset + words.length}/{total}
-          </strong>{' '}
-          words
-        </span>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold">Từ điển Anh Việt</h1>
+        <div className="flex items-center justify-between">
+          <span>
+            Showing{' '}
+            <strong className="font-bold">
+              {offset} - {offset + words.length}/{total}
+            </strong>{' '}
+            words
+          </span>
 
-        <div>
-          Words per page{' '}
-          <Select className="py-1 px-2" value={limit} onChange={doChangeLimit}>
-            <option value="100">100</option>
-            <option value="200">200</option>
-            <option value="500">500</option>
-            <option value="1000">1000</option>
-          </Select>
+          <div>
+            Words per page{' '}
+            <Select
+              className="py-1 px-2"
+              value={limit}
+              onChange={doChangeLimit}>
+              <option value="100">100</option>
+              <option value="200">200</option>
+              <option value="500">500</option>
+              <option value="1000">1000</option>
+            </Select>
+          </div>
         </div>
-      </div>
-      <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
-        {words.map(({ word }) => (
-          <LinkToWord
-            key={word}
-            query={{ word }}
-            className="rounded bg-slate-100 p-2">
-            {word}
-          </LinkToWord>
-        ))}
+
+        <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
+          {words.map(({ word }) => (
+            <LinkToWord
+              mergeQuery={false}
+              key={word}
+              query={{ word }}
+              className="rounded bg-slate-100 p-2">
+              {word}
+            </LinkToWord>
+          ))}
+        </div>
+
+        <div className="flex items-center gap-2">
+          Change to page:
+          <TruncatedPagination
+            currentPage={currentPage}
+            totalPage={totalPage}
+            LinkComponent={PageLink}
+          />
+        </div>
       </div>
     </Layout>
   )
