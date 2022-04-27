c8bae7175cec8f6c589b66dd6c499df87b384df1
Dang Min <dang.min@tecnotree.com>
2022-04-27 11:30:55 +0300

    Add FlashcardInstruction.

---
+++ web/lib/components/word/FlashcardInstruction.jsx
@@ -0,0 +1,39 @@
+import { useTranslation } from '../../i18n'
+import { getStopShowFlashcardInstruction } from '../../storage'
+import { LightbulbIcon } from '../common/Icons'
+
+export function FlashcardInstructionAction() {
+  const { _e } = useTranslation()
+  const value = getStopShowFlashcardInstruction().get()
+
+  const onChange = (e) =>
+    getStopShowFlashcardInstruction().set(e.target.checked)
+
+  return (
+    <div className="flex items-center gap-2 text-xs text-slate-300">
+      <input
+        className="!border-slate-300"
+        type="checkbox"
+        value={value}
+        onChange={onChange}
+      />
+      {_e('flashcard.instructions.doNotShowFlashcardInstructionAgain')}
+    </div>
+  )
+}
+
+export function FlashcardInstructionTitle() {
+  const { _e } = useTranslation()
+  return (
+    <div className="flex gap-2">
+      <LightbulbIcon size="16px" /> {_e('common.tips')}
+    </div>
+  )
+}
+
+export function FlashcardInstructionDescription() {
+  const { _e } = useTranslation()
+  return (
+    <p className="text-sm">{_e('flashcard.instructions.clickOnDefinition')}</p>
+  )
+}
