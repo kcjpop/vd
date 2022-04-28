15a1720a80613573484af8a6054bc5b13e47893a
Dang Min <dang.min@tecnotree.com>
2022-04-28 09:18:01 +0300

    Add queries to add function and triggers to validate user_id of flashcards before modify.

---
+++ supabase/migrations/validate_flashcard_user_id.sql
@@ -0,0 +1,25 @@
+create or replace function validate_upsert_flashcard_fn() returns trigger as $fn$
+  declare
+    setUserId uuid;
+  begin
+    select user_id into setUserId from public.flashcard_sets where id=new.set_id;
+    
+    if setUserId <> new.user_id then
+      raise exception 'User not allow to modify flashcard in the other flashcard_set';
+    end if;
+
+    return new;
+  end;
+$fn$ language plpgsql;
+
+create trigger validate_before_insert_flashcard_trigger
+  before INSERT
+  on public.flashcards
+  FOR EACH row
+  execute function validate_upsert_flashcard_fn();
+
+create trigger validate_before_update_flashcard_trigger
+  before update
+  on public.flashcards
+  FOR EACH row
+  execute function validate_upsert_flashcard_fn();
