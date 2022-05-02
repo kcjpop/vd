d441779a207f97a8125ce650f0684cb22ca3e03a
Dang Min <stea.steven@gmail.com>
2022-05-02 21:53:04 +0300

    Clear storage after logout. (#81)
    * Clear storage after logout.
    
    * Assume our code is perfect! Well said!

--- web/lib/auth.js
+++ web/lib/auth.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { supabase } from './supabaseClient'
+import { clearStorage } from './storage'
 
 export function register({ email, password, fullname }) {
   return supabase.auth.signUp(
@@ -16,8 +17,10 @@ export function login({ email, password }) {
   return supabase.auth.signIn({ email, password })
 }
 
-export function logout() {
-  return supabase.auth.signOut()
+export async function logout() {
+  await supabase.auth.signOut()
+
+  clearStorage()
 }
 
 export function useUser({ redirectIfUnauthenticated = false } = {}) {
