a36f293012584ecc1c32d849bf707895b999fb41
Dang Min <dang.min@tecnotree.com>
2022-05-07 20:15:57 +0300

    Add functions to useSingleSet.

--- web/lib/components/flashcards/useSingleSet.js
+++ web/lib/components/flashcards/useSingleSet.js
@@ -1,15 +1,18 @@
-import { useQuery } from 'react-query'
-import { getFlashcardSet } from './query'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { getFlashcardSet, upsertFlashcard, deleteFlashcards } from './query'
 
 export function useSingleSet(
   { user, setId },
   { fields } = { fields: 'id, user_id, flashcards(*)' },
 ) {
+  const queryKey = ['flashcard-set', user?.id, setId]
+  const queryClient = useQueryClient()
+
   const {
     data: currentSet,
     isLoading,
     isError,
-  } = useQuery(['flashcard-set', user?.id, setId], async () => {
+  } = useQuery(queryKey, async () => {
     if (!setId || !user) return
 
     const { data } = await getFlashcardSet(
@@ -23,5 +26,49 @@ export function useSingleSet(
     return data
   })
 
-  return { currentSet, isLoading, isError }
+  // @todo: Add user_id as parameter
+  const createNewFlashcard = useMutation(
+    async ({ word, definition, setId }) => {
+      const { error } = await upsertFlashcard({ word, definition })
+
+      if (error) throw error
+    },
+    {
+      mutationKey: ['create-flashcard', setId],
+      onSuccess: () => queryClient.invalidateQueries(queryKey),
+    },
+  )
+
+  const updateFlashcard = useMutation(
+    async ({ id, word, definition, setId }) => {
+      const { error } = await upsertFlashcard({ id, word, definition, setId })
+
+      if (error) throw error
+    },
+    {
+      mutationKey: ['update-flashcard', setId],
+      onSuccess: () => queryClient.invalidateQueries(queryKey),
+    },
+  )
+
+  const deleteFlashcard = useMutation(
+    async ({ id }) => {
+      const { error } = await deleteFlashcards({ id })
+
+      if (error) throw error
+    },
+    {
+      mutationKey: ['delete-flashcard', setId],
+      onSuccess: () => queryClient.invalidateQueries(queryKey),
+    },
+  )
+
+  return {
+    currentSet,
+    isLoading,
+    isError,
+    createNewFlashcard,
+    updateFlashcard,
+    deleteFlashcard,
+  }
 }
